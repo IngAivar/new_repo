a315d47f779f3910f2c1a645c6d333e79c360daf
MiniGan067 <MiniGan067@yandex.ru>
2023-02-11 19:26:27 +0300

    update str_func.py add docstring and new func

--- str_func.py
+++ str_func.py
@@ -2,9 +2,19 @@
 
 def all_upper(string):
     """
-        Эта функция получает строку и возвращает её в верхнем регистре
+    Эта функция получает строку и возвращает её в верхнем регистре
     """
 
     upper_string = string.upper()
 
     return upper_string
+
+
+def all_title(string):
+    """
+    Эта функция получает строку и возвращает её так что каждая первая буква в слове будет написана в верхнем регистре
+    """
+
+    titled_string = string.title()
+
+    return titled_string
